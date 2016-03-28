5f1e6801db9789085b33649373f74689d2a35f7f
Adam Jacob Muller <adam@isprime.com>
2016-03-28 18:47:50 -0400

    unwatch (unscrobble) all items in a given playlist

---
+++ unwatch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import requests
+import datetime
+import time
+import argparse
+import json
+
+import xml.etree.ElementTree as ET
+#import lxml.etree as etree
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--address", help="address of plex")
+parser.add_argument("--playlist", help="playlist id")
+args = parser.parse_args()
+
+url = "%s/playlists/%s/items" % (args.address, args.playlist)
+print url
+items = requests.get(url)
+
+
+items_xml = ET.fromstring(items.text.encode("ascii", errors="ignore"))
+
+videos = items_xml.findall("./Video")
+
+avt = []
+
+for video in videos:
+    #print json.dumps(video.attrib, indent=4)
+    if 'grandparentTitle' in video.attrib:
+        name = '%s - %s' % (video.attrib['grandparentTitle'], video.attrib['title'])
+    else:
+        name = '%s' % (video.attrib['title'])
+
+    res = requests.get("%s/:/unscrobble?identifier=com.plexapp.plugins.library&key=%s" % (args.address, video.attrib['ratingKey']))
+
+    print "%s %s[%s] = %s" % (video.attrib.get("originallyAvailableAt"), name, video.attrib['playlistItemID'], str(res))
